b4db262a37e569bcb09e864526d0283d3f6c4f61
Ivan12581 <32292274+Ivan12581@users.noreply.github.com>
2020-07-09 18:30:29 +0800

    123

--- TestProject/Assets/Editor/IosSDKSetting.cs
+++ TestProject/Assets/Editor/IosSDKSetting.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
-#if UNITY_EDITOR_OSX
+//#if UNITY_EDITOR_OSX
 using UnityEditor.iOS.Xcode;
-#endif
+//#endif
 using System.IO;
 using System.Linq;
 using UnityEditor;
 using System;
 public class IosSDKSetting : Editor{
-    #if UNITY_EDITOR_OSX
+    //#if UNITY_EDITOR_OSX
     public const string path = "/Users/mini/Documents/EmptyXcodeprj";
     [MenuItem("IosSDKSetting/FaceBook配置")]
     public static void IOSbuildFaceBook()
@@ -126,7 +126,51 @@ public class IosSDKSetting : Editor{
 
     }
 
+    [MenuItem("IosSDKSetting/FBGP配置")]
+    public static void IOSbuildFBGP()
+    {
 
-#endif
+        //添加XCode引用的Framework
+        string projPath = PBXProject.GetPBXProjectPath(path);
+        PBXProject proj = new PBXProject();
+        proj.ReadFromString(File.ReadAllText(projPath));
+        string target = proj.TargetGuidByName("Unity-iPhone");
+        // SDK依赖
+        proj.AddFrameworkToProject(target, "LocalAuthentication.framework", false);
+        proj.AddFrameworkToProject(target, "SafariServices.framework", false);
+        proj.AddFrameworkToProject(target, "AuthenticationServices.framework", false);
+        proj.AddFrameworkToProject(target, "SystemConfiguration.framework", false);
+        // BuildSetting修改
+        //proj.SetBuildProperty(target, "ENABLE_BITCODE", "NO");
+        proj.AddBuildProperty(target, "OTHER_LDFLAGS", "-ObjC");
+        proj.AddBuildProperty(target, "OTHER_LDFLAGS", "-all_load");
+
+        string plistPath = Path.Combine(path, "Info.plist");
+        PlistDocument plist = new PlistDocument();
+        plist.ReadFromFile(plistPath);
+        PlistElementDict rootDict = plist.root;
+        rootDict.SetString("FacebookAppID", "949004278872387");
+        rootDict.SetString("FacebookAppDisplayName", "Girl for the Throne TW");
+        // URL types配置
+        PlistElementArray URLTypes = plist.root.CreateArray("CFBundleURLTypes");
+        string[] urlSchemes = { "fb949004278872387" , "com.googleusercontent.apps.554619719418-rtqb4au05hj99h8h6n70i6b8i3d91tun" };
+        foreach (string str in urlSchemes)
+        {
+            PlistElementDict typeRole = URLTypes.AddDict();
+            typeRole.SetString("CFBundleTypeRole", "Editor");
+            PlistElementArray urlScheme = typeRole.CreateArray("CFBundleURLSchemes");
+            urlScheme.AddString(str);
+        }
+        // LSApplicationQueriesSchemes配置
+        PlistElementArray LSApplicationQueriesSchemes = plist.root.CreateArray("LSApplicationQueriesSchemes");
+        // facebook接入配置为了适配ios9
+        LSApplicationQueriesSchemes.AddString("fbapi");
+        LSApplicationQueriesSchemes.AddString("fb-messenger-share-api");
+        LSApplicationQueriesSchemes.AddString("fbauth2");
+        LSApplicationQueriesSchemes.AddString("fbshareextension");
+        plist.WriteToFile(plistPath);
+    }
+
+    //#endif
 }
 
