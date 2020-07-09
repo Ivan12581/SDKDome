f1b287d831748de6dc2d0db7a6123934efffc095
Xiongyongping <1094135209@qq.com>
2020-07-09 20:24:02 +0800

    123

---
+++ TestProject/Assets/Plugins/IOS/GoogleHelper.h
@@ -0,0 +1,14 @@
+//
+//  GoogleHelper.h
+//  Unity-iPhone
+//
+//  Created by mini on 7/9/20.
+//
+#import <GoogleSignIn/GoogleSignIn.h>
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+@interface GoogleHelper:NSObject<UIApplicationDelegate, GIDSignInDelegate>
+-(void)InitSDK;
+-(void)Login;
++(id)sharedInstance;
+@end
