39d6159a97f66c7509f24998bd038e69a9792c36
Ivan12581 <32292274+Ivan12581@users.noreply.github.com>
2020-07-09 20:24:27 +0800

    123

--- TestProject/Assets/Scripts/Test/LoginView.cs
+++ TestProject/Assets/Scripts/Test/LoginView.cs
@@ -71,7 +71,6 @@ public class LoginView : MonoBehaviour
 
         });
     }
-
     public void ApplePay() {
         Debug.Log("---Unity---SDKPay---");
         SDKPay.gi.Pay("test1");
@@ -102,27 +101,9 @@ public class LoginView : MonoBehaviour
     public void GCLogin()
     {
         Debug.Log("---Unity---GCLogin---");
-        SDKManager.gi.Login(SDKLoginType.GameCenter,(s, dataDict) => {
+        SDKManager.gi.Login(SDKLoginType.FaceBook,(s, dataDict) => {
             Debug.Log("---Unity---GCLogin--callback-");
-            //int state = int.Parse(dataDict["state"]);
 
-            c2a_logon_apple_gamecenter args = new c2a_logon_apple_gamecenter();
-            dataDict.TryGetValue("state", out string state);
-            dataDict.TryGetValue("playerID", out string playerID);
-            dataDict.TryGetValue("publicKeyUrl", out string publicKeyUrl);
-            dataDict.TryGetValue("signature", out string signature);
-            dataDict.TryGetValue("salt", out string salt);
-            dataDict.TryGetValue("timestamp", out string timestamp);
-            args.UserIdentifier = playerID;
-            //应服务器要求 下面字段先注释掉 正式应该加上的
-            //args.PublicKeyUrl = publicKeyUrl;
-            //args.Signature = signature;
-            //args.Salt = salt;
-            //args.Timestamp = timestamp;
-            if (int.Parse(state) == 1)
-            {
-                NetworkManager.gi.ConnectAuth_LoginGameCenter(args);
-            }
         });
     }
     public void FBLogin()
@@ -156,11 +137,7 @@ public class LoginView : MonoBehaviour
     public void FBShare() {
         Debug.Log("---Unity---FBShare---");
     }
-    public void ApplePayInit()
-    {
-        //Debug.Log("---Unity---ApplePayInit---");
-        //SDKPay.gi.ApplePayInit();
-    }
+
     public void Switch()
     {
         Debug.Log("---Unity---Switch---");
