c1f361be4ab1f649225887bc4428062a0c216ea2
Ivan12581 <32292274+Ivan12581@users.noreply.github.com>
2020-08-24 11:05:24 +0800

    【 同步 】

--- TestProject/Assets/Plugins/Android/Utils.java
+++ TestProject/Assets/Plugins/Android/Utils.java
@@ -7,6 +7,8 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.preference.PreferenceManager;
 import android.provider.Settings;
@@ -16,7 +18,13 @@ import android.util.Base64;
 import androidx.annotation.RequiresApi;
 import androidx.annotation.RequiresPermission;
 
+
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.security.MessageDigest;
 import java.util.Currency;
 import java.util.Locale;
@@ -47,18 +55,18 @@ public class Utils {
     //Build.VERSION_CODES.M 23
     //Build.VERSION_CODES.O 26
     //Build.VERSION_CODES.Q 29
-    @SuppressLint("HardwareIds")
+//    @SuppressLint("HardwareIds")
     public String getIMEIDeviceId() {
         mainActivity.ShowLog("---getIMEIDeviceId--->");
         String deviceId;
         String uniqueID = UUID.randomUUID().toString();
         mainActivity.ShowLog("---Build.VERSION.SDK_INT--->" + Build.VERSION.SDK_INT);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+        if (Build.VERSION.SDK_INT >= 29)
         {
             deviceId = Settings.Secure.getString(mainActivity.getContentResolver(), Settings.Secure.ANDROID_ID);
         } else {
             final TelephonyManager mTelephony = (TelephonyManager) mainActivity.getSystemService(Context.TELEPHONY_SERVICE);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Build.VERSION.SDK_INT >= 23) {
                 if (mainActivity.checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
                     return "";
                 }
@@ -66,7 +74,7 @@ public class Utils {
             assert mTelephony != null;
             if (mTelephony.getDeviceId() != null)
             {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                if (Build.VERSION.SDK_INT >= 26)
                 {
                     mainActivity.ShowLog( "---getImei-->");
                     deviceId = mTelephony.getImei();
@@ -126,5 +134,48 @@ public class Utils {
         SharedPreferences.Editor editor = preferences.edit();
         //存储数据时选用对应类型的方法
         return preferences.getString(key,"");
+    }
+    //去除小数点后多余的0 或者小数点
+    public String rvZeroAndDot(String s){
+        if (s.isEmpty()) {
+            return null;
+        }
+        if(s.indexOf(".") > 0){
+            s = s.replaceAll("0+?$", "");//去掉多余的0
+            s = s.replaceAll("[.]$", "");//如最后一位是.则去掉
+        }
+        return s;
+    }
+    public boolean isNoEmpty(String str){
+        if(str == null || str.length() == 0){
+            return  false;
+        }
+        return true;
+    }
+    /**
+     * 根据图片的url路径获得Bitmap对象
+     * @param url
+     * @return
+     */
+    public Bitmap returnBitmap(String url) {
+        URL fileUrl = null;
+        Bitmap bitmap = null;
+        try {
+            fileUrl = new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try {
+            HttpURLConnection conn = (HttpURLConnection) fileUrl
+                    .openConnection();
+            conn.setDoInput(true);
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            bitmap = BitmapFactory.decodeStream(is);
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
     }
 }
