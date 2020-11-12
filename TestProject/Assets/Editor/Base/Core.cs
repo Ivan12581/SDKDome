3d4b63bc0eecb32372700755fc43d636636d6dc3
Ivan12581 <32292274+Ivan12581@users.noreply.github.com>
2020-11-12 18:34:03 +0800

    1

--- TestProject/Assets/Editor/Base/Core.cs
+++ TestProject/Assets/Editor/Base/Core.cs
@@ -1,8 +1,47 @@
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Core 
 {
-
+    private static bool IsUTF8Bytes(byte[] data)
+    {
+        int charByteCounter = 1; //计算当前正分析的字符应还有的字节数 
+        byte curByte; //当前分析的字节. 
+        for (int i = 0; i < data.Length; i++)
+        {
+            curByte = data[i];
+            if (charByteCounter == 1)
+            {
+                if (curByte >= 0x80)
+                {
+                    //判断当前 
+                    while (((curByte <<= 1) & 0x80) != 0)
+                    {
+                        charByteCounter++;
+                    }
+                    //标记位首位若为非0 则至少以2个1开始 如:110XXXXX...........1111110X 
+                    if (charByteCounter == 1 || charByteCounter > 6)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                //若是UTF-8 此时第一位必须为1 
+                if ((curByte & 0xC0) != 0x80)
+                {
+                    return false;
+                }
+                charByteCounter--;
+            }
+        }
+        if (charByteCounter > 1)
+        {
+            throw new Exception("非预期的byte格式");
+        }
+        return true;
+    }
 }
